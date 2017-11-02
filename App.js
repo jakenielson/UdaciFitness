7e180d3d4b3fcdbbbc7595d7da29911eef486b0d
jakenielson <groakayt@gmail.com>
2017-11-02 15:33:40 -0700

    Create submit button

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
+  handlePress = () => {
+    alert('Hello!');
+  }
+
   render() {
     return (
-      <View >
+      <View style={styles.container}>
         <AddEntry />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  btnText: {
+    color: '#fff'
+  }
+});
